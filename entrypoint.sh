f658215bef902d64e00d0a369368af074ab07881
Dagen Brock <dagenbrock@gmail.com>
2020-02-10 15:53:51 -0600

    named env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ M32=https://github.com/digarok/merlin32/releases/download/v1.1.8d/merlin32-ubunt
 curl -L $M32 -o merlin32.zip
 unzip merlin32.zip
 mv merlin32 /usr/bin
-sh -c "cd $1 ; /usr/bin/merlin32 -V $3 $2"
+sh -c "cd $INPUT_SOURCE_DIR ; /usr/bin/merlin32 -V $INPUT_SOURCE_FILE $INPUT_MACRO_DIR"
