75615cac428fb317f70cf6f4b8e87777da261198
Dagen Brock <dagenbrock@gmail.com>
2020-02-10 15:42:31 -0600

    updated

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get update
 
 # Install packages
 #RUN apt-get -yq install rsync openssh-client
+RUN apt-get -yq install unzip curl
 
 
 # Label
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ M32=https://github.com/digarok/merlin32/releases/download/v1.1.8d/merlin32-ubunt
 
 curl -L $M32 -o merlin32.zip
 unzip merlin32.zip
-sudo mv merlin32 /usr/bin
+mv merlin32 /usr/bin
 sh -c "cd $1 ; /usr/bin/merlin32 -V $3 $2"
